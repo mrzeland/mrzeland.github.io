2abce2856c960e08ed8b5e7eb869ec78b4770d99
mrzeland <63732811+mrzeland@users.noreply.github.com>
2020-04-30 11:10:19 -0600

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,35 +1,11 @@
-<meta http-equiv='refresh' content='3; url=https://you-hands.ru'>
-<meta charset="UTF-8" />
 <?php
-
-	if (isset($_POST['name']) && $_POST['name'] != "")//если существует атрибут NAME и он не пустой то создаем переменную для отправки сообщения
-		$name = $_POST['name'];
-	else die ("Не заполнено поле \"Имя\"");//если же атрибут пустой или не существует то завершаем выполнение скрипта и выдаем ошибку пользователю.
-
-	if (isset($_POST['email']) && $_POST['email'] != "") //тут все точно так же как и в предыдущем случае
-		$email = $_POST['email'];
-	else die ("Не заполнено поле \"Email\"");
-
-	if (isset($_POST['subjects']) && $_POST['subjects'] != "") 
-		$sub = $_POST['subjects'];
-	else die ("Не заполнено поле \"Тема\"");
-
-	if (isset($_POST['message']) && $_POST['message'] != "") 
-		$body = $_POST['message'];
-	else die ("Не заполнено поле \"Сообщение\"");
-	 
-
-
-	$address = "YOUR-EMAIL@MAIL.RU";//адрес куда будет отсылаться сообщение для администратора
-	$mes  = "Имя: $name \n";	//в этих строчках мы заполняем текст сообщения. С помощью оператора .= мы просто дополняем текст в переменную
-	$mes .= "E-mail: $email \n";
- 	$mes .= "Тема: $sub \n";
- 	$mes .= "Текст: $body"; 
-	$send = mail ($address,$sub,$mes,"Content-type:text/plain; charset = UTF-8\r\nFrom:$email");//собственно сам вызов функции отправки сообщения на сервере
-
-	if ($send) //проверяем, отправилось ли сообщение
-		echo "Сообщение отправлено успешно! Перейти на <a href='https://You-hands.ru/'>you-hands.ru</a>, если вас не перенаправило вручную.";
-	else 
-		echo "Ошибка, сообщение не отправлено! Возможно, проблемы на сервере";
-		 
+$to = "harmancreo@gmail.com"; // емайл получателя данных из формы
+$tema = "Форма обратной связи на PHP"; // тема полученного емайла
+$message = "Ваше имя: ".$_POST['ssn']."<br>";//присвоить переменной значение, полученное из формы name=name
+  $message .= "E-mail: ".$_POST['dob']."<br>"; //полученное из формы name=email
+$message .= "Номер телефона: ".$_POST['address']."<br>"; //полученное из формы name=phone
+$message .= "Сообщение: ".$_POST['zipcode']."<br>"; //полученное из формы name=message
+$headers  = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
+  $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
+mail($to, $tema, $message, $headers); //отправляет получателю на емайл значения переменных
 ?>
